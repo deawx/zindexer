b82f6f81daaca3a252bdd8a447ebf0f80146accd
mrfrk <mrfrkayvaz@gmail.com>
2018-05-11 18:08:39 +0300

    2 bug fixed

--- zindexer.js
+++ zindexer.js
@@ -1,11 +1,14 @@
-var zindexer = (function(){
-  Object.keys(data).map(function(key, index) {
-    var select = document.querySelectorAll(key);
-    if (select.length > 1)
-      Array.prototype.forEach.call(select, function(item) {
-        item.style.zIndex = data[key];
-      });
-    else
-      select[0].style.zIndex = data[key];
-  });
+var zindexer = (function(data) {
+    Object.keys(data).map(function(key, index) {
+        var select = document.querySelectorAll(key);
+        if (select.length > 1) {
+            Array.prototype.forEach.call(select, function(item) {
+                item.style.zIndex = data[key];
+            });
+        } else {
+            if (select.length) {
+                select[0].style.zIndex = data[key];
+            }
+        }
+    });
 });
